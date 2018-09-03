eecc94f3862eb975e319bc46a271a82dd35858c4
eltNEG <yorewole@gmail.com>
2018-09-03 14:04:10 +0100

    configure entrypoint server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+"user strict";
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+routes(app);
+app.listen(port);
+
+console.log(`Serving app on: http://localhost:${port}`);
