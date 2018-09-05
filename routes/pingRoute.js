bd29461bc5e7af0cb1aad98b443a37e38a297df6
eltNEG <yorewole@gmail.com>
2018-09-06 00:38:58 +0100

    change filename

--- routes/pingRoute.js
+++ routes/pingRoute.js
@@ -1,9 +1,7 @@
 import express from 'express';
 
-const pingRoute = express.Router();
+export const pingRoute = express.Router();
 
 const pingController = (req, res) => res.status(200).send('pong');
 
 pingRoute.route('/ping').get(pingController);
-
-export default pingRoute;
