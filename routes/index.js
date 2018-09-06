74e0dcf4ba496a208ffc18c9f80705aca599fa51
eltNEG <eltNEG@users.noreply.github.com>
2018-09-06 13:51:14 +0100

    Api v1 implemented (#14)
    * Initialise ui folder with empty files and folders
    
    Creates a directory structure in the ui folder. index.html file, css, images
    js folders were created to hold html, styles, images and javascript files
    
    * create and add basic html element for sign-up
    
    * add style to sign-up.html elements
    
    * refactor code for styling
    
    * add type to input, modified sign-in link
    
    * create styled sign-in page from sign-up page
    
    * complete auth pages for user
    
    * Initialise ui folder with empty files and folders
    
    Creates a directory structure in the ui folder. index.html file, css, images
    js folders were created to hold html, styles, images and javascript files
    
    * create and add basic html element for sign-up
    
    * add style to sign-up.html elements
    
    * refactor code for styling
    
    * add type to input, modified sign-in link
    
    * create styled sign-in page from sign-up page
    
    * add style for non mobile screen size
    
    * auto format html
    
    * add order-food page, elements and style class
    
    * add style for order-food page
    
    * remove unused div
    
    * add order-history page with style class attributes
    
    * add style for order-history page
    
    * add manage-order page with elements
    
    * add style for manage-order.html
    
    * add manage-food-item page
    
    * style manage-food-item page
    
    * auto format document, added new attribute to input element
    
    * increase form input font size, add more class attribute  styles
    
    * re-styled manage order table
    
    * rename html css class attributes, add update table button
    
    * rewrite page title, add meta description
    
    * Update readme
    
    added header, project overview, features, contribution guide and todo to readme
    
    * change badge and fix features' links
    
    * fix sign up button's url
    
    * fix sign in button's url
    
    * map links to the correct url
    
    * Implement Fast-Food-Fast Api v1 (#13)
    
    * set up directories for api with empty files
    
    * add babel env settings
    
    * ignore node_modules folder
    
    * Add dependencies and scripts
    
    * yarn.lock file added
    
    * configure entrypoint server.js
    
    * setup routes and ping route for test
    
    * correct start script
    
    * correct typo
    
    * init eslint with airbnb rules
    
    * add .gitattribute to prevent automatic conversion of line endings
    
    * add more eslint dependencies and script
    
    * ignore yarn-error.log
    
    * update file
    
    * auto format with eslint
    
    * add .gitattribute to prevent automatic conversion of line endings
    
    * auto format with eslint
    
    * auto format with eslint
    
    * remove .gitattributes
    
    * add jasmine to env
    
    * add jasmine and requests to test
    
    * add status code 200
    
    * update
    
    * add config for jasmine test
    
    * export server
    
    * add test for ping route
    
    * ignore dist
    
    * change env to mocha
    
    * add coverage script for intanbul and mocha
    
    * update
    
    * remove unused file
    
    * auto format with eslint
    
    * add coveralls to coverage script
    
    * ignore coverage folder
    
    * add coveralls
    
    * update
    
    * configure travis
    
    * add eslint ignore
    
    * add coverall and travis badge
    
    * ignore .coveralls.yml
    
    * change branch in build travis badge
    
    * fix ui doesn't exit error on travis
    
    * Implement routes (#12)
    
    * Add cov script to prevent clean up locally
    
    * Convert request body to json
    
    * Made little change
    
    * change filename
    
    * Add tests for OrderRoutes
    
    * implement order route
    
    * implement controllers for order route
    
    * Implement order model
    
    * update with orderRoute
    
    * Format and refactor code with eslint
    
    * Moved order test from test-orderRoute to test-orderModel
    
    * add node version for heroku
    
    * Add heroku procfile
    
    * change line endings to lf
    
    * add update
    
    * change line ending to LF
    
    * Remove procfile
    
    * add Procfile
    
    * Move babel from devdependencies to dependencies
    
    * Change node version
    
    * Configure notifications

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import pingRoute from './pingRoute';
+import orderRoute from './orderRoute';
+
+const apiVersion = '/api/v1';
+
+const routes = (app) => {
+  app.use(apiVersion, pingRoute);
+  app.use(apiVersion, orderRoute);
+};
+
+export default routes;
