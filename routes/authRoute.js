8457535c745cd1da29ca55ca7f9a525324d22481
eltNEG <yorewole@gmail.com>
2018-09-30 21:02:38 +0100

    Add authRoute

---
+++ routes/authRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { loginUser, createUser } from '../controller/authController';
+
+
+const authRoute = express.Router();
+
+authRoute.route('/auth/signup').post(createUser);
+
+authRoute.route('/auth/login').post(loginUser);
+
+export default authRoute;
