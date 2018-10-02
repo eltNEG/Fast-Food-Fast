986e962cbc88b3f8c2584ed456a500befe6d0fac
eltNEG <yorewole@gmail.com>
2018-10-02 15:57:04 +0100

    Re-implement order route

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,14 +1,22 @@
 import express from 'express';
-import controller from '../controller';
+import ordersController from '../controller/ordersController';
+import jwtAuthenticator from '../customMiddleware/jwtAuthenticator';
+import onlyAdmin from '../customMiddleware/onlyAdmin';
 
 const orderRoute = express.Router();
 
-orderRoute.route('/orders')
-  .get(controller.ordersController)
-  .post(controller.newOrderController);
+orderRoute
+  .route('/orders')
+  .get(jwtAuthenticator, onlyAdmin, ordersController.getOrders)
+  .post(jwtAuthenticator, ordersController.postOrders);
 
-orderRoute.route('/order/:orderId')
-  .get(controller.getOrderController)
-  .put(controller.updateStatusController);
+orderRoute
+  .route('/order/:orderId')
+  .get(jwtAuthenticator, onlyAdmin, ordersController.getSpecificOrder)
+  .put(ordersController.updateOrderStatus);
+
+orderRoute
+  .route('/users/:userId/orders')
+  .get(ordersController.getSpecificUserOrder);
 
 export default orderRoute;
