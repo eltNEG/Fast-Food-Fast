daa4967da9e1247b80ad6f007dd763b6a6183317
eltNEG <yorewole@gmail.com>
2018-10-02 15:49:53 +0100

    Made little changes

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config({ silent: true })
+dotenv.config({ silent: true });
+
+export default () => console.log(`Node env: ${process.env.NODE_ENV}`);
