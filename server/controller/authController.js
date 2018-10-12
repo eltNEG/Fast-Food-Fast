1be659d88647c46db2e06ba0835dcb5423563b84
eltNEG <yorewole@gmail.com>
2018-10-13 00:39:30 +0100

    Restructure api response - add data key

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -11,20 +11,28 @@ export const createUser = (req, res) => {
     });
   }
   const query = `
-      INSERT INTO Users (username, password, role) VALUES($1, $2, $3) RETURNING userid;
+      INSERT INTO Users (username, password, role) VALUES($1, $2, $3) RETURNING *;
       `;
   const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync());
   return db.getClient().then(client => client
     .query(query, [username, hashedPassword, 'user'])
-    .then(dbRes => res.status(201).json({
-      success: true,
-      message: 'user succesfully created',
-      userId: dbRes.rows[0].userid,
-    }))
-    .catch(({ message }) => {
+    .then((dbRes) => {
+      const user = dbRes.rows[0];
+      return res.status(201).json({
+        success: true,
+        message: 'user succesfully created',
+        data: {
+          user,
+          token: jwt.sign(user, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+          }),
+        },
+      });
+    })
+    .catch(() => {
       res.status(400).json({
         success: false,
-        message,
+        message: 'db error - sign up not successful',
       });
     })
     .then(client.release()));
@@ -60,10 +68,12 @@ export const loginUser = (req, res) => {
         return res.status(200).json({
           success: true,
           message: 'user found',
-          user: authUser,
-          token: jwt.sign(authUser, process.env.JWT_SECRET, {
-            expiresIn: '1h',
-          }),
+          data: {
+            user: authUser,
+            token: jwt.sign(authUser, process.env.JWT_SECRET, {
+              expiresIn: '1h',
+            }),
+          },
         });
       }
       return res.status(400).json({
@@ -71,9 +81,9 @@ export const loginUser = (req, res) => {
         message: 'invalid password',
       });
     })
-    .catch(({ message }) => res.status(400).json({
+    .catch(() => res.status(400).json({
       success: false,
-      message,
+      message: 'db error - login not successful',
     }))
     .then(() => client.release()));
 };
