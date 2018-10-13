b68d1ebe47ac64ef7d2941c0cdc39f4042103c62
eltNEG <yorewole@gmail.com>
2018-10-13 01:37:35 +0100

    Add validator

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { loginUser, createUser } from '../controller/authController';
+import validate from '../customMiddleware/validator';
 
 
 const authRoute = express.Router();
 
-authRoute.route('/auth/signup').post(createUser);
+authRoute.route('/auth/signup').post(validate, createUser);
 
-authRoute.route('/auth/login').post(loginUser);
+authRoute.route('/auth/login').post(validate, loginUser);
 
 export default authRoute;
