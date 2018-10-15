f3156d196c9fc641b1c9cb059c25b5d757e4a575
eltNEG <yorewole@gmail.com>
2018-10-15 06:47:47 +0100

    Create public folder in server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ loadConfig();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(validateURI);
