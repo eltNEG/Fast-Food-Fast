1415372a9cb5824be06fc0d9efb7905c8d8a496b
eltNEG <yorewole@gmail.com>
2018-10-18 15:23:00 +0100

    Add api documentation

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ loadConfig();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('UI'))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
