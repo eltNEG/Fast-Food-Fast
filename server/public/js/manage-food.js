dc150ead775e71849c11af81e841f545988abbcf
eltNEG <yorewole@gmail.com>
2018-10-19 07:53:57 +0100

    Add jquery and reconfigure eslint

--- server/public/js/manage-food.js
+++ server/public/js/manage-food.js
@@ -1,27 +1,34 @@
 const baseUrl = 'http://localhost:3000/api/v1';
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const foodNameElement = document.getElementById('food-name');
-  const imgUrlElement = document.getElementById('img-url');
+// const handleSubmit = (event) => {
+//   event.preventDefault();
+//   const foodNameElement = document.getElementById('food-name');
+//   const imgUrlElement = document.getElementById('img-url');
 
-  const foodName = foodNameElement.value;
-  const imgUrl = imgUrlElement.value;
-  const data = {
-    menu: foodName,
-    imgUrl,
-  };
-  const url = `${baseUrl}/menu`;
-  const options = {
-    method: 'POST',
-    body: data,
-    headers: new Headers(),
-    json: true,
-  };
+//   const foodName = foodNameElement.value;
+//   const imgUrl = imgUrlElement.value;
+//   const data = {
+//     menu: foodName,
+//     imgUrl,
+//   };
+//   const url = `${baseUrl}/menu`;
+//   const options = {
+//     method: 'POST',
+//     body: data,
+//     headers: new Headers(),
+//     json: true,
+//   };
 
-  console.log(options);
+//   console.log(options);
 
-  fetch(url).then((res) => {
-    console.log(response);
+//   fetch(url).then((res) => {
+//     console.log(res);
+//   });
+// };
+
+$(document).ready(() => {
+  $('#btn').click((event) => {
+    event.preventDefault();
+    console.log('clicked');
   });
-};
+});
