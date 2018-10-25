e7321831a4424920a02c25d637bd6b335a5a1829
eltNEG <yorewole@gmail.com>
2018-10-25 01:05:52 +0100

    Add PUT and DELETE /menu/foodid routes, exlint format

--- server/controller/menuController.js
+++ server/controller/menuController.js
@@ -31,7 +31,7 @@ const postMenu = (req, res) => {
   // query
   // change food to menu
   const query = `
-            INSERT INTO Foods (foodName, url) VALUES ($1) RETURNING *;
+            INSERT INTO Foods (foodName, url) VALUES ($1, $2) RETURNING *;
         `;
   return db.getClient().then(client => client
     .query(query, [menu, imgUrl])
@@ -41,14 +41,71 @@ const postMenu = (req, res) => {
       data: {
         menu: dbRes.rows,
       },
-    })).catch(() => res.status(400).json({
-      success: false,
-      message: 'db error - add menu item not successful',
-    }))
+    })).catch((err) => {
+      console.log(err);
+      res.status(400).json({
+        success: false,
+        message: 'db error - add menu item not successful',
+      });
+    })
     .then(() => client.release()));
+};
+
+const updateMenu = (req, res) => {
+  let query; let
+    param;
+  const { foodId } = req.params;
+  const { menu, imgUrl } = req.body;
+
+  // query
+  /** change orderState to orderStatus */
+  if (menu) {
+    query = `
+    UPDATE Foods 
+    SET foodName = $2
+    WHERE foodId = $1 RETURNING *;
+      `;
+    param = menu;
+  } if (imgUrl) {
+    query = `
+    UPDATE Foods 
+    SET url = $2
+    WHERE foodId = $1 RETURNING *;
+      `;
+    param = imgUrl;
+  }
+
+
+  // query the database and handle response
+  return db.getClient().then((client) => {
+    client.query(query, [foodId, param]).then(dbRes => res.status(200).json({
+      success: true,
+      message: 'Menu succesfully updated',
+      data: {
+        menu: dbRes.rows,
+      },
+    })).then(client.release());
+  });
+};
+
+const deleteMenu = (req, res) => {
+  const { foodId } = req.params;
+
+  const query = `
+    DELETE FROM Foods
+    WHERE foodId = $1;
+  `;
+  return db.getClient().then((client) => {
+    client.query(query, [foodId]).then(() => res.status(200).json({
+      success: true,
+      message: 'Menu succesfully deleted',
+    })).then(client.release());
+  });
 };
 
 export default {
   getMenu,
   postMenu,
+  updateMenu,
+  deleteMenu,
 };
--- server/public/js/manage-order.js
+++ server/public/js/manage-order.js
@@ -1,21 +1,21 @@
-const userid = localStorage.getItem("fffUser")
-const token = localStorage.getItem("fffToken")
+const userid = localStorage.getItem('fffUser');
+const token = localStorage.getItem('fffToken');
 
 const loadOrders = () => {
-    const url = `http://localhost:3000/api/v1/users/${userid}/orders`
+  const url = `http://localhost:3000/api/v1/users/${userid}/orders`;
 
-    const options = {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
-          authorization: token,
-        },
-      };
-    fetch(url, options).then(res => res.json()).then(jsonData => {
-        if(jsonData.success) {
-            jsonData.data.orders.forEach(order => {
-                $("tbody").append(`
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      authorization: token,
+    },
+  };
+  fetch(url, options).then(res => res.json()).then((jsonData) => {
+    if (jsonData.success) {
+      jsonData.data.orders.forEach((order) => {
+        $('tbody').append(`
                 <tr>
                 <td>${order.customername}</td>
                 <td>${order.foodordered}</td>
@@ -33,10 +33,10 @@ const loadOrders = () => {
                     ${order.orderstatus}
                 </td>
             </tr>
-                `)
-            })
-        }
-    })
-}
+                `);
+      });
+    }
+  });
+};
 
-loadOrders()
+loadOrders();
